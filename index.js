a3e856728b5812f4b69d5505d8255e58a9275b19
flowswarm <brezzythetrapper@gmail.com>
2025-05-22 23:11:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer'); // keep this
+console.log('Puppeteer version:', puppeteer);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
