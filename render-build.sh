05b2990d66f58f4ae64163c6a0d6819668b5b8f0
flowswarm <brezzythetrapper@gmail.com>
2025-05-23 00:25:41 -0400

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,4 +2,3 @@
 echo "Forcing Puppeteer to reinstall Chrome..."
 rm -rf ~/.cache/puppeteer
 npx puppeteer browsers install chrome --force
-ls ~/.cache/puppeteer/chrome/*/chrome-linux64 || echo "Chrome not found!"
